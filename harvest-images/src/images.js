7b88301d798939e3dd5df4caa88bd1e8ab4d3034
Rainer Simon <aboutgeo@gmail.com>
2022-06-15 15:23:54 +0200

    Image harvesting script

---
+++ harvest-images/src/images.js
@@ -0,0 +1,69 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+import { parseIiif } from '@allmaps/iiif-parser';
+
+const WAIT_MS = 300;
+
+const pad = (num, size) => {
+  let str = num.toString();
+  while (str.length < size) str = '0' + str;
+  return str;
+}
+
+/**
+ * Harvests all images for the given manifest file
+ */
+export const harvestImages = async (barcode, manifestPath, imageDir) => {
+  const str = fs.readFileSync(manifestPath, 'utf8');
+
+  const { images } = parseIiif(JSON.parse(str));
+
+  console.log(`Harvesting ${barcode} (${images.length})`);
+  
+  const harvestOne = (idx = 0) => {
+    const { label, fullImageUrl, format } = images[idx];
+
+    const filename = `${imageDir}${pad(idx + 1, 5)}_${label}.${format}`;
+
+    if (fs.existsSync(filename)) {
+      console.log(`Skipping ${label} (${idx + 1}/${images.length}) - exists`);
+      
+      if ((idx + 1) < images.length)
+        return harvestOne(idx + 1);
+    } else {
+      console.log(`Downloading ${label} (${idx + 1}/${images.length})`);
+      return fetch(fullImageUrl)
+        .then(response => new Promise(resolve => {
+          const fileStream = fs.createWriteStream(filename + '.download');
+          response.body.pipe(fileStream);
+              
+          response.body.on('error', err => {
+            // Just log, keep going
+            console.log('Error downloading ' + fullImageUrl);
+            resolve(err);
+          });
+          
+          fileStream.on('finish', () => {
+            fs.stat(filename + '.download', (err, stats) => {
+              if (stats.size < 1000) {
+                console.error('Error downloading ' + fullImageUrl);
+              } else {
+                fs.renameSync(filename + '.download', filename);
+                console.log(`Wrote file ${filename}`);
+              }
+            });
+
+            resolve();
+          })
+        }))
+        .then(() => {
+          if ((idx + 1) < images.length) {
+            return new Promise(r => setTimeout(r, WAIT_MS))
+              .then(() => harvestOne(idx + 1));
+          }
+        })
+    }
+  }
+
+  return harvestOne();
+}
---
+++ harvest-images/src/index.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+
+import { harvestImages } from "./images.js";
+
+const MANIFEST_FOLDER = '../data/manifests/D16/';
+const IMAGE_FOLDER = '../data/images/D16/'
+
+const files = fs.readdirSync(MANIFEST_FOLDER);
+
+/**
+ * Harvests images for one work.
+ */
+const harvestOneWork = async (idx = 0) => {
+  const filename = files[idx];
+  const barcode = filename.split('.')[0];
+
+  // Images go into a sub-folder named after the barcode
+  const imageDir = `${IMAGE_FOLDER}${barcode}/`; 
+  if (!fs.existsSync(imageDir))
+    fs.mkdirSync(imageDir);
+
+  harvestImages(barcode, `${MANIFEST_FOLDER}${filename}`, imageDir).then(() => {
+    if ((idx + 1) < files.length) {
+      harvestOneWork(idx + 1);
+    } else {
+      console.log('Done');
+    }
+  });
+}
+
+harvestOneWork();
